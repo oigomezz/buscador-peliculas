978a7b715124c4b9f206db852b73439a21e717a1
Oscar Ivan Gomez Zamudio <oigomezz@unal.edu.co>
2024-04-19 17:47:11 -0500

    Se actualiza .gitignore y movies.js para usar variables de entorno .env

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,21 +1,22 @@
-const API_KEY = 'c997ecfc'
+const apikey = import.meta.env.VITE_API_KEY;
 
 export const searchMovies = async ({ search }) => {
-  if (search === '') return null
+  if (search === "") return null;
 
   try {
-    const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
-    const json = await response.json()
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=${apikey}&s=${search}`
+    );
+    const json = await response.json();
+    const movies = json.Search;
 
-    const movies = json.Search
-
-    return movies?.map(movie => ({
+    return movies?.map((movie) => ({
       id: movie.imdbID,
       title: movie.Title,
       year: movie.Year,
-      image: movie.Poster
-    }))
+      image: movie.Poster,
+    }));
   } catch (e) {
-    throw new Error('Error searching movies')
+    throw new Error("Error searching movies");
   }
-}
+};
